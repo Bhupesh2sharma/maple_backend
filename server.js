cff6b2be590fda307fc865758ca3a6ae86041a8f
bhupesh <bhupeshxrma24233@gmail.com>
2025-02-19 19:24:54 +0530

    fix : cors issue

--- server.js
+++ server.js
@@ -9,7 +9,25 @@ const availabilityRoutes = require("./routes/availability.js");
 const bookingRoutes = require("./routes/bookingRoutes");
 // Middleware
 app.use(express.json());
-app.use(cors());
+
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://maple-t-t.vercel.app",
+    
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+};
+
+
+app.use(cors(corsOptions));
 
 // Routes
 
